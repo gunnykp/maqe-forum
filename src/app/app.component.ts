df150f97168836e8abc5016ef11312c6416c1b87
gunnykp <44629117+gunnykp@users.noreply.github.com>
2024-05-17 18:28:17 +0700

    push ForumPostComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ForumPostComponent } from './forum-post/forum-post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [ForumPostComponent],
+  template: '<app-forum-post></app-forum-post>',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
